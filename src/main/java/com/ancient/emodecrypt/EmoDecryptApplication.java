2a7a4f8f3b75cbd0a9fa52b165979a4f7e8bb680
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-27 20:06:35 -0300

    Projeto inicial

---
+++ src/main/java/com/ancient/emodecrypt/EmoDecryptApplication.java
@@ -0,0 +1,13 @@
+package com.ancient.emodecrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoDecryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmoDecryptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ancient/emodecrypt/EmoDecryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ancient.emodecrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoDecryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
