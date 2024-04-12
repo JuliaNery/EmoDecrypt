ba53c99dec7a923c269f8e1a7673bda873e7cf8f
JuliaNery <julianerysinigalia@gmail.com>
2024-04-12 02:13:21 -0300

    feat(): integração com OpenIA

---
+++ src/main/java/com/ancient/emodecrypt/controller/MassaDadosController.java
@@ -0,0 +1,9 @@
+package com.ancient.emodecrypt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("MassaDados")
+public class MassaDadosController {
+}
--- src/main/java/com/ancient/emodecrypt/entity/MassaDadosEntity.java
+++ src/main/java/com/ancient/emodecrypt/entity/MassaDadosEntity.java
@@ -7,16 +7,23 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class MassaDadosEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
     private String comentario;
+    private List<String> palavrasChaves;
+    private String emocaoTransmitida;
+    private Integer nivelSatisfacao;
+    private Integer qtdCurtidas;
 }
